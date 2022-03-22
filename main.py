55c4670ec4a6c809381e203f0b0c7994712e6c73
Joey <jholm6332@dysartstudents.org>
2022-03-22 16:25:18 -0700

    minecraft module added removed pycraft

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import getpass
-import sys
-import re
-from optparse import OptionParser
-
-from pyCraft.minecraft import authentication
-from pyCraft.minecraft.exceptions import YggdrasilError
-from pyCraft.minecraft.networking.connection import Connection
-from pyCraft.minecraft.networking.packets import Packet, clientbound, serverbound
 
 
 def main():
